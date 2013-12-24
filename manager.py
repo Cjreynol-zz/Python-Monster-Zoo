1f29fa533605b8f42bbec0a23ced528a6b7d3d1f
Cjreynol <cjreynol13@aol.com>
2013-12-24 17:30:03 -0500

    Updated Manager_Menu.
    -Did away with properties, manager doesn't need to keep track of the monster's stats.
    -Now it pulls the value for each update method, sets the label, and the locals go away

--- manager.py
+++ manager.py
@@ -42,149 +42,118 @@ class Manager_Menu:
 		
 		self.monster = Monster(name)
 
-		# mood
+		########### mood ############################################
 		self.mood_label = Label(self.root, text = "Mood:  ")
 		self.mood_label.grid(row = 0, column = 0)
+
 		self.mood_state = StringVar()
 		self.mood_state.set(self.monster.mood)
 		Label(self.root, textvariable = self.mood_state).grid(row = 0, column = 1)
 
-		# hunger
-		self._hunger = self.monster.hunger
+		######### hunger ############################################
 		self.hunger_label = Label(self.root, text = "Hunger:  ")
 		self.hunger_label.grid(row = 1, column = 0)
+
 		self.hunger_state = StringVar()
-		self.hunger_state.set(self.hunger)
+		self.update_hunger()
 		Label(self.root, textvariable = self.hunger_state).grid(row = 1, column = 1)
+
 		self.hunger_button = Button(self.root, text = "Feed", command = self.feed)
 		self.hunger_button.grid(row = 1, column = 2)
 
-		# sleepiness
-		self._sleepiness = self.monster.sleepiness
+		######## sleepiness ############################################
 		self.sleep_label = Label(self.root, text = "Sleepiness:  ")
 		self.sleep_label.grid(row = 2, column = 0)
+
 		self.sleep_state = StringVar()
-		self.sleep_state.set(self.sleepiness)
+		self.update_sleepiness()
 		Label(self.root, textvariable = self.sleep_state).grid(row = 2, column = 1)
+
 		self.sleep_button = Button(self.root, text = "Nap", command = self.nap)
 		self.sleep_button.grid(row = 2, column = 2)
 
-		# boredom
-		self._boredom = self.monster.boredom
+		######### boredom ############################################
 		self.boredom_label = Label(self.root, text = "Boredom:  ")
 		self.boredom_label.grid(row = 1, column = 3)
+
 		self.boredom_state = StringVar()
-		self.boredom_state.set(self.boredom)
+		self.update_boredom()
 		Label(self.root, textvariable = self.boredom_state).grid(row = 1, column = 4)
+
 		self.boredom_button = Button(self.root, text = "Play", command = self.play)
 		self.boredom_button.grid(row = 1, column = 5)
 
-		# dirtiness
-		self._dirtiness = self.monster.dirtiness
+		######### dirtiness ############################################
 		self.dirt_label = Label(self.root, text = "Dirtiness:  ")
 		self.dirt_label.grid(row = 2, column = 3)
+
 		self.dirt_state = StringVar()
-		self.dirt_state.set(self.dirtiness)
+		self.update_dirtiness()
 		Label(self.root, textvariable = self.dirt_state).grid(row = 2, column = 4)
+
 		self.dirt_button = Button(self.root, text = "Clean", command = self.clean)
 		self.dirt_button.grid(row = 2, column = 5)
 		
 		self.root.mainloop()
 
-	@property
-	def hunger(self):
-		"""Gets a string descriptor for the monster's hunger."""
-		if self._hunger >= 2 * Monster.HUNGRY:
-			return "High"
-		elif self._hunger >= Monster.HUNGRY:
-			return "Medium"
-		else:
-			return "Low"
-
-	@hunger.setter
-	def hunger(self, value):
-		"""Sets the value for the hunger property."""
-		self._hunger = value
-
 	def update_hunger(self):
-		"""Updates the hunger label through the hunger property."""
-		self.hunger = self.monster.hunger
-		self.hunger_state.set(self.hunger)
+		"""Updates the hunger label by generating a string from the monster's hunger field."""
+		value = self.monster.hunger	
+		if value >= 2 * Monster.HUNGRY:
+			text = "High"
+		elif value >= Monster.HUNGRY:
+			text = "Medium"
+		else:
+			text = "Low"
+		self.hunger_state.set(text)
 
 	def feed(self):
 		"""Feeds the monster."""
 		self.monster.feed()
 		self.update_all()
 
-	@property
-	def sleepiness(self):
-		"""Gets a string descriptor for the monster's sleepiness."""
-		if self._sleepiness >= 2 * Monster.SLEEPY:
-			return "High"
-		elif self._sleepiness >= Monster.SLEEPY:
-			return "Medium"
-		else:
-			return "Low"
-
-	@sleepiness.setter
-	def sleepiness(self, value):
-		"""Sets the value for the sleepiness property."""
-		self._sleepiness = value
-
 	def update_sleepiness(self):
-		"""Updates the sleepiness label through the sleepiness property."""
-		self.sleepiness = self.monster.sleepiness
-		self.sleep_state.set(self.sleepiness)
+		"""Updates the sleepiness label by generating a string from the monster's sleepiness field."""
+		value = self.monster.sleepiness
+		if value >= 2 * Monster.SLEEPY:
+			text = "High"
+		elif value >= Monster.SLEEPY:
+			text = "Medium"
+		else:
+			text = "Low"
+		self.sleep_state.set(text)
 
 	def nap(self):
 		"""Puts the monster down for a nap."""
 		self.monster.nap()
 		self.update_all()
 
-	@property
-	def boredom(self):
-		"""Gets a string descriptor for the mosnter's boredom."""
-		if self._boredom >= 2 * Monster.BORED:
-			return "High"
-		elif self._boredom >= Monster.BORED:
-			return "Medium"
-		else:
-			return "Low"
-
-	@boredom.setter
-	def boredom(self, value):
-		"""Sets the value for the boredom property."""
-		self._boredom = value
-
 	def update_boredom(self):
-		"""Updates the boredom label through the boredom property."""
-		self.boredom = self.monster.boredom
-		self.boredom_state.set(self.boredom)
+		"""Updates the boredom label by generating a string from the monster's boredom field."""
+		value = self.monster.boredom
+		if value >= 2 * Monster.BORED:
+			text = "High"
+		elif value >= Monster.BORED:
+			text = "Medium"
+		else:
+			text = "Low"
+		self.boredom_state.set(text)
 
 	def play(self):
 		"""Entertains the monster."""
 		self.monster.play()
 		self.update_all()
 
-	@property
-	def dirtiness(self):
-		"""Gets a string descriptor for the monster's dirtiness."""
-		if self._dirtiness >= 2 * Monster.DIRTY:
-			return "High"
-		elif self._dirtiness >= Monster.DIRTY:
-			return "Medium"
-		else:
-			return "Low"
-
-	@dirtiness.setter
-	def dirtiness(self, value):
-		"""Sets the value for the dirtiness property."""
-		self._dirtiness = value
-
 	def update_dirtiness(self):
-		"""Updates the dirtiness label through the dirtiness property."""
-		self.dirtiness = self.monster.dirtiness
-		self.dirt_state.set(self.dirtiness)
+		"""Updates the dirtiness label by generating a string from the monster's dirtiness field."""
+		value = self.monster.dirtiness
+		if value >= 2 * Monster.DIRTY:
+			text = "High"
+		elif value >= Monster.DIRTY:
+			text = "Medium"
+		else:
+			text = "Low"
+		self.dirt_state.set(text)
 
 	def clean(self):
 		"""Cleans the monster."""
