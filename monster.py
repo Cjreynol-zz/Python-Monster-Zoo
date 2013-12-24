5884f13142117e937613c9f17f6b6514bcb91a25
Cjreynol <cjreynol13@aol.com>
2013-12-23 21:57:19 -0500

    Created the monster class.
    -It has a name, four stats (hunger, boredom, sleepiness, dirtiness) and an overall mood derived from these stats.
    -The four methods have default values, but the goal is to have the minigames decide what value to pass in.
    
    ++Next to create is the monster manager, which is a form to create a monster, then call it's four methods.
    	++Then a monster viewer, that displays a picture based on the current mood of the monster.
    		++Then create a single minigame to display, probably take_nap()

---
+++ monster.py
@@ -0,0 +1,89 @@
+# Chad Reynolds
+# 12/23/13
+# MonsterZoo, monster.py has the class for creating your monsters
+
+class Monster:
+	"""A virtual monster to take care of."""
+	total = 0
+	HUNGRY = 5
+	BORED = 4
+	DIRTY = 5
+	SLEEPY = 6
+	SAD = 14
+
+	def __init__(self, name):
+		self._name = name
+		self.hunger = 0
+		self.boredom = 0
+		self.dirtiness = 0
+		self.sleepiness = 0
+		self._mood = "happy"
+		total += 1
+
+	def __str__(self):
+		return "{0} is currently {1}.".format(name, mood)
+
+	@property
+	def name(self):
+		"""Get the monster's name."""
+		return _name
+
+	@name.setter
+	def name(self, value):
+		"""Set the monster's name."""
+		if value == "":
+			raise ValueError("Cannot have a monster with an empty name.")
+		else:
+			_name = value
+
+	@property
+	def mood(self):
+		"""Get the current mood."""
+		if self.hunger + self.boredom + self.dirtiness + self.sleepiness > SAD:
+			self._mood = "sad"
+		elif self.hunger > HUNGRY:
+			self._mood = "hungry"
+		elif self.sleepiness > SLEEPY:
+			self._mood = "sleepy"
+		elif self.boredom > BORED:
+			self._mood = "bored"
+		elif self.dirtiness > DIRTY:
+			self._mood = "dirty"
+		else:
+			self._mood = "happy"
+		return self._mood
+
+	def _pass_time(self):
+		"""Shows the passage of time on the monster as events occur."""
+		self.hunger += 1
+		self.boredom += 1
+		self.dirtiness += 1
+		self.sleepiness += 1
+
+	def feed_monster(self, food = 3):
+		"""Feeds the monster an amount of food."""
+		self._pass_time()
+		self.hunger -= food
+		if self.hunger < 0:
+			self.hunger = 0
+
+	def nap_time(self, rest = 6):
+		"""Gives the monster a nap for number of hours."""
+		self._pass_time()
+		self.sleepiness -= rest
+		if self.sleepiness < 0:
+			self.sleepiness = 0
+
+	def play_time(self, fun = 3):
+		"""Plays with the monster for a number of hours."""	
+		self._pass_time()
+		self.boredom -= fun
+		if self.boredom < 0:
+			self.boredom = 0	
+
+	def give_bath(self, soap = 5):
+		"""Cleans the monster off."""
+		self._pass_time()
+		self.dirtiness -= soap
+		if self.dirtiness < 0:
+			self.dirtiness = 0
